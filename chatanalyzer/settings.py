cb49ad899612fb5ef897d8f43f7f8462cd82d264
Philipp Kundratitz <philippkundratitz@MacBook-Pro-Philipp.local>
2020-03-14 16:49:39 +0100

    added static root

--- chatanalyzer/settings.py
+++ chatanalyzer/settings.py
@@ -128,6 +128,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
