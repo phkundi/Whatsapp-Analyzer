0c0a29d6d1967cff6ba914b7708fbb9cf7d1d92a
Philipp Kundratitz <philippkundratitz@MacBook-Pro-Philipp.local>
2020-03-14 17:35:14 +0100

    settings

--- chatanalyzer/settings.py
+++ chatanalyzer/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'd238924298fh2293232rh0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['whatsappanalyse.herokuapp.com']
 
